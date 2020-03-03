0937a68a48c36f1f3cca773d8eb75840c9a4ec89
Aaron Roller <aaron.roller@automodality.com>
2020-03-02 20:53:38 -0800

    moving make to action AM-24/pcl-version

---
+++ .github/actions/build@local/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e # fail on error
+
+mkdir release
+cd release
+cmake -DCMAKE_BUILD_TYPE=None -DCMAKE_INSTALL_PREFIX=/usr \
+    -DBUILD_GPU=OFF -DBUILD_apps=OFF -DBUILD_examples=OFF \
+    -DCMAKE_INSTALL_PREFIX=/usr ..
+make
+sudo make install
+
